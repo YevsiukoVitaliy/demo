bdcaf64b24c19928f633114b392d204f8d8f33ec
Yevsiukov Vitalii <eulhas9@gmail.com>
2023-07-21 14:07:13 +0300

    лдж

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const cookieSession = require("cookie-session");
 const express = require('express');
 const cors = require('cors');
@@ -7,6 +6,7 @@ const passportSetup = require("./passport");
 const passport = require("passport");
 const fileUpload = require('express-fileupload');
 const path = require('path');
+const httpProxy = require('http-proxy');
 
 const sequelize = require('./db/db');
 const models = require('./models/models');
@@ -16,23 +16,14 @@ const authRoute = require("./routes/authRoute");
 const app = express();
 const host = process.env.HOST;
 const port = process.env.PORT;
-
+const httpServer = require('http').createServer(app);
 
 app.use(
   cookieSession({ name: "session", keys: [process.env.SECRET_KEY ], maxAge: 24 * 60 * 60 * 100 })
 );
 
-
 app.use(passport.initialize());
 app.use(passport.session());
-
-app.use((req, res, next) => {
-  if (req.protocol === 'http') {
-    res.redirect(301, `https://${req.headers.host}${req.url}`);
-  } else {
-    next();
-  }
-});
 
 app.use(
   cors({
@@ -42,7 +33,6 @@ app.use(
   })
 );
 
-
 app.use("/auth", authRoute);
 
 // middleware
@@ -51,20 +41,22 @@ app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload({}));
 app.use('/api', router);
 
-
-// Handler error, last middleware
 app.use(errorHandler);
 
 const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
+    app.all('*', (req, res) => {
+      proxy.web(req, res, { target: 'https://nodejsclusters-115724-0.cloudclusters.net' });
+    });
 
-    app.listen(port, () => {
-      console.log(process.env.CLIENT_URL);
+    httpServer.listen(port, () => {
+      console.log(`Сервер запущен на порту ${port}`);
     });
   } catch (e) {
     console.error(e);
   }
 };
+
 start().then(() => console.log('Starting...'));
