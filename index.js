af46194188535b47704659a7d79304a421d6e523
Yevsiukov Vitalii <93053290+YevsiukoVitaliy@users.noreply.github.com>
2023-07-21 22:28:14 +0300

    sadsad

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const cookieSession = require("cookie-session");
+const https = require('https');
+const fs = require('fs');
+const cookieSession = require('cookie-session');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const passportSetup = require("./passport");
-const passport = require("passport");
+const passportSetup = require('./passport');
+const passport = require('passport');
 const fileUpload = require('express-fileupload');
 const path = require('path');
-const httpProxy = require('http-proxy');
 
 const sequelize = require('./db/db');
 const models = require('./models/models');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
-const authRoute = require("./routes/authRoute");
+const authRoute = require('./routes/authRoute');
 const app = express();
 const host = process.env.HOST;
 const port = process.env.PORT;
-const httpServer = require('http').createServer(app);
 
 app.use(
-  cookieSession({ name: "session", keys: [process.env.SECRET_KEY ], maxAge: 24 * 60 * 60 * 100 })
+  cookieSession({ name: 'session', keys: [process.env.SECRET_KEY], maxAge: 24 * 60 * 60 * 100 })
 );
 
 app.use(passport.initialize());
@@ -28,14 +28,13 @@ app.use(passport.session());
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
-    methods: "GET,POST,PUT,DELETE",
+    methods: 'GET,POST,PUT,DELETE',
     credentials: true,
   })
 );
 
-app.use("/auth", authRoute);
+app.use('/auth', authRoute);
 
-// middleware
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload({}));
@@ -43,20 +42,19 @@ app.use('/api', router);
 
 app.use(errorHandler);
 
+const sslOptions = {
+  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')), // Provide the path to your private key file
+  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')), // Provide the path to your SSL certificate file
+};
+
+const httpsServer = https.createServer(sslOptions, app);
+
 const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
 
-    // Создаем экземпляр прокси-сервера
-    const proxy = httpProxy.createProxyServer();
-
-    // Проксируем все запросы на ваше приложение через HTTPS
-    app.all('*', (req, res) => {
-      proxy.web(req, res, { target: 'https://nodejsclusters-115724-0.cloudclusters.net' });
-    });
-
-    httpServer.listen(port, () => {
+    httpsServer.listen(port, () => {
       console.log(`Сервер запущен на порту ${port}`);
     });
   } catch (e) {
