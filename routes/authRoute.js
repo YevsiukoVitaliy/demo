948cc11fb8218fd5511c601ec88c4b1998b95387
Yevsiukov Vitalii <93053290+YevsiukoVitaliy@users.noreply.github.com>
2023-07-22 18:36:20 +0300

    sda

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,17 +2,43 @@ const router = require("express").Router();
 const passport = require("passport");
 const { generateJwt } = require("../passport");
 
+// Добавляем функцию для выполнения fetch-запроса
+const fetchUserData = async () => {
+  try {
+    const response = await fetch('https://nodejsclusters-115724-0.cloudclusters.net/auth/login/success');
+    // Проверяем, успешен ли запрос (статус 200-299)
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error; // Перевыбрасываем ошибку, чтобы её можно было обработать вне этой функции
+  }
+};
+
+// Внутри вашего роутера вызываем функцию fetchUserData
 router.get("/login/success", async (req, res) => {
   try {
     if (req.user) {
       const token = await generateJwt(req.user.id, req.user.email, req.user.role);
 
       const { password, ...userWithoutPassword } = req.user;
+
+      // Вызываем функцию fetchUserData для получения данных
+      const userData = await fetchUserData();
+
+      // Кодирование данных, полученных из fetch-запроса
+      const { dataField1, dataField2 } = userData;
+
       res.status(200).json({
         success: true,
         message: "successfull",
         user: userWithoutPassword,
-        token: token, 
+        token: token,
+        dataField1, // Включаем полученные данные в ответ на роутере
+        dataField2,
       });
     } else {
       res.status(401).json({
@@ -28,6 +54,7 @@ router.get("/login/success", async (req, res) => {
     });
   }
 });
+
 
 
 router.get("/login/failed", (req, res) => {
