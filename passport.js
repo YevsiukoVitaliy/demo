7d11a2774f82a7dc3d374ceff536d6cea1f54ba5
Yevsiukov Vitalii <93053290+YevsiukoVitaliy@users.noreply.github.com>
2023-07-24 01:22:36 +0300

    ывф

--- passport.js
+++ passport.js
@@ -26,6 +26,7 @@ passport.use(
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
       callbackURL: "/auth/google/callback",
+      scope: ['profile', 'email']
     },
     async (accessToken, refreshToken, profile, done) => {
       const email = profile.emails[0].value;
