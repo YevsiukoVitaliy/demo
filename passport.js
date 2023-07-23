2ed6b9a8d87b23e439bf62b701b5d7540f577fe5
Yevsiukov Vitalii <93053290+YevsiukoVitaliy@users.noreply.github.com>
2023-07-24 01:44:12 +0300

    ывф

--- passport.js
+++ passport.js
@@ -5,14 +5,13 @@ const passport = require("passport");
 const { User, Basket } = require("./models/models");
 const jwt = require('jsonwebtoken');
 
-
 const GOOGLE_CLIENT_ID = "219729734091-43l16e04kigm8susjpfifbc92q5su4k1.apps.googleusercontent.com";
 const GOOGLE_CLIENT_SECRET = "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";
 const MICROSOFT_CLIENT_ID = "22a8d34f-e1e5-4ed1-8ce4-809377001a33";
 const MICROSOFT_CLIENT_SECRET = "Gyr8Q~2UozcGrc0StXbpo2TeBGpglPTUxlgtLb6U";
 const FACEBOOK_APP_ID = "207933322242984";
 const FACEBOOK_APP_SECRET = "00243e005980a2228a767f32b1a81313";
-const SECRET_KEY = process.env.SECRET_KEY 
+const SECRET_KEY = process.env.SECRET_KEY;
 
 const generateJwt = (id, email, role) => {
   return jwt.sign({ id, email, role }, SECRET_KEY, {
@@ -25,8 +24,7 @@ passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
-      scope: ['profile', 'email']
+      callbackURL: "https://nodejsclusters-115724-0.cloudclusters.net/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       const email = profile.emails[0].value;
@@ -53,7 +51,7 @@ passport.use(
     {
       clientID: MICROSOFT_CLIENT_ID,
       clientSecret: MICROSOFT_CLIENT_SECRET,
-      callbackURL: "/auth/microsoft/callback",
+      callbackURL: "https://nodejsclusters-115724-0.cloudclusters.net/auth/microsoft/callback",
       scope: ['user.read'],
     },
     async (accessToken, refreshToken, profile, done) => { 
@@ -81,7 +79,7 @@ passport.use(
     {
       clientID: FACEBOOK_APP_ID,
       clientSecret: FACEBOOK_APP_SECRET,
-      callbackURL: "/auth/facebook/callback",
+      callbackURL: "https://nodejsclusters-115724-0.cloudclusters.net/auth/facebook/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       const email = profile.emails[0].value;
