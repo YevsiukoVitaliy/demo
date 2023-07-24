3b4ba446d400ea11c61d29eb1f2aa01b5f509a23
Yevsiukov Vitalii <93053290+YevsiukoVitaliy@users.noreply.github.com>
2023-07-24 03:58:03 +0300

    sda

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const passport = require("passport");
 const { generateJwt } = require("../passport");
 
-router.get("/login/success", async (req, res) => {
+router.get("https://nodejsclusters-115724-0.cloudclusters.net/auth/login/success", async (req, res) => {
   try {
     if (req.user) {
       const token = await generateJwt(req.user.id, req.user.email, req.user.role);
@@ -30,29 +30,29 @@ router.get("/login/success", async (req, res) => {
 });
 
 
-router.get("/login/failed", (req, res) => {
+router.get("https://nodejsclusters-115724-0.cloudclusters.net/auth/login/failed", (req, res) => {
   res.status(401).json({
     success: false,
     message: "failure",
   });
 });
 
-router.get("/logout", (req, res) => {
+router.get("https://nodejsclusters-115724-0.cloudclusters.net/auth/logout", (req, res) => {
   req.logout();
   res.redirect(process.env.CLIENT_URL);
 });
 
-router.get("/google", passport.authenticate("google", { scope: ["openid", "profile", "email"] }));
+router.get("https://nodejsclusters-115724-0.cloudclusters.net/auth/google", passport.authenticate("google", { scope: ["openid", "profile", "email"] }));
 
 router.get(
-  "/google/callback",
+  "https://nodejsclusters-115724-0.cloudclusters.net/auth/google/callback",
   passport.authenticate("google", {
     successRedirect: process.env.CLIENT_URL,
     failureRedirect: "https://nodejsclusters-115724-0.cloudclusters.net/auth/login/failed",
   })
 );
 
-router.get("/microsoft", passport.authenticate("microsoft", { scope: ["openid", "profile", "email"] }));
+router.get("https://nodejsclusters-115724-0.cloudclusters.net/auth/microsoft", passport.authenticate("microsoft", { scope: ["openid", "profile", "email"] }));
 
 router.get(
   "/microsoft/callback",
@@ -62,7 +62,7 @@ router.get(
   })
 );
 
-router.get("/facebook", passport.authenticate("facebook", { scope: ["openid", "profile", "email"] }));
+router.get("https://nodejsclusters-115724-0.cloudclusters.net/auth/facebook", passport.authenticate("facebook", { scope: ["openid", "profile", "email"] }));
 
 router.get(
   "/facebook/callback",
